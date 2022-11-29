b17bacbc350be88bf5249245a20235e3c4e47163
Mezie-Gift <iammeziegift@gmail.com>
2022-11-29 15:52:43 -0800

    alphabet with exceptions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for alphabet in range(ord('a'), ord('z') + 1):
     if alphabet == ord('e') or alphabet == ord('q'):
         continue
-        print("{:c}".format(alphabet), end="")
+    print("{:c}".format(alphabet), end="")
