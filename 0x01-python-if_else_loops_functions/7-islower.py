bea38278975f5e0dc854dfb7146d30daa54c6cb7
Mezie-Gift <iammeziegift@gmail.com>
2022-11-30 16:09:31 -0800

    check for lowercase

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def islower(c):
+    if (ord(c) >= 97 and ord(c) <= 122):
+        return True
+    else:
+        return False
+"""ctr = 0
+for lower in c:
+    if(ord(c) >= 97 and ord(c) <= 122):
+        ctr = ctr + 1
+if ctr > 0:
+    return True
+else:
+   return False
+	"""
