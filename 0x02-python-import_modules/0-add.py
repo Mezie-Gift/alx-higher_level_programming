3868c5c6f0f429fb2a885c93175fa698f6e4adb9
Mezie-Gift <iammeziegift@gmail.com>
2022-12-01 16:32:25 -0800

    sum up

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 #AUTHOR: Mezie Gift
-add = __import__('add_0').add
-
-a = 1
-b = 2
-add(a, b)
-print("{} {} {} {} {}".format(a, '+', b, '=', add(a, b)))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} {} {} {} {}".format(a, '+', b, '=', add(a, b)))
