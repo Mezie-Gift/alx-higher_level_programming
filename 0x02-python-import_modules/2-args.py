256f88d894db384d821b755d7e88111bdb419e8b
Mezie-Gift <iammeziegift@gmail.com>
2022-12-02 05:39:57 -0800

    print argument

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# AUTHOR: Mezie Gift
+
+"""a program that prints the number of and the list of its arguments"""
+if __name__ == "__main__":
+    import sys
+    lent = len(sys.argv)
+    if lent <= 1:
+        print("0 argument.")
+    else:
+        if lent == 2:
+            print("{:d} argument:".format(lent - 1))
+        else:
+            print("{:d} arguments:".format(lent - 1))
+        for i in range(1, lent):
+            print("{:d}: {}".format(lent, sys.argv[i]))
