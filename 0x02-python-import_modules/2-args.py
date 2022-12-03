85b857915d8bf41259c8d234b8447016f1d018fc
Mezie-Gift <iammeziegift@gmail.com>
2022-12-03 14:32:23 -0800

    command line manipulation

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
         else:
             print("{:d} arguments:".format(lent - 1))
         for i in range(1, lent):
-                print("{:d}: {}".format(i, sys.argv[i]))
+            print("{:d}: {}".format(i, sys.argv[i]))
