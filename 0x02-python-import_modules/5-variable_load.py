65846ab2e3e8bdc1568cc643977b9054b7213141
Mezie-Gift <iammeziegift@gmail.com>
2022-12-04 12:01:13 -0800

    import variable from a module

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# AUTHOR: Mezie Gift
+
+if __name__ == "__main__":
+    """imports the variable a from the file
+    variable_load_5.py and prints its value."""
+    from variable_load_5 import a
+    print("{:d}".format(a))
