418c98374b3e4ff42145e60d0e1e27db16f8dc30
Mezie-Gift <iammeziegift@gmail.com>
2022-12-06 12:57:02 -0800

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,9 @@
 # AUTHOR: Mezie Gift
 """This function prints a matrix"""
 
+
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
             print("{:d}".format(col), end=" " if col != row[-1] else "")
         print()
-
