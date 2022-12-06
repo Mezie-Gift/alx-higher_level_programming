bc7cea497b9a44cd5fca25704c75d1da82cdac91
Mezie-Gift <iammeziegift@gmail.com>
2022-12-05 16:26:36 -0800

    print list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # AUTHOR: Mezie Gift
 def print_list_integer(my_list=[]):
-    result = '{}\n{}\n{}\n{}\n{}'.format(*my_list)
-    print(result)
+    result = my_list
+    print('{}\n{}\n{}\n{}\n{}'.format(*result))
