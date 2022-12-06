9e1b5c0919a5a486dc73b057df3e11d4514e2608
Mezie-Gift <iammeziegift@gmail.com>
2022-12-06 05:09:18 -0800

    print numbers in reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# AUTHOR: Mezie Gift
+def print_reversed_list_integer(my_list=[]):
+    """Prints lisy of integers in reverse"""
+    for num in reversed(my_list):
+        print("{}".format(num))
