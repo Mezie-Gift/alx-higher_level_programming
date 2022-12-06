521eae72aebf93dc8a5a8b2706f47a403e157bce
Mezie-Gift <iammeziegift@gmail.com>
2022-12-06 05:28:41 -0800

    print nunbers of a list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@
 def print_reversed_list_integer(my_list=[]):
     """Prints lisy of integers in reverse"""
     for num in reversed(my_list):
-        print("{}".format(num))
+        print("{:d}".format(num))
