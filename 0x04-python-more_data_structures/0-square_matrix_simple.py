8f18f83967e27fa8ed5e459899fc2957c4e15898
Mezie-Gift <iammeziegift@gmail.com>
2022-12-07 14:08:06 -0800

    print squares of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,6 +5,7 @@ def square_matrix_simple(matrix=[]):
 
 
     matrix_sqr = matrix.copy()
+
     for i in range(len(matrix)):
         matrix_sqr[i] = list(map(lambda x: x**2, matrix[i]))
     return (matrix_sqr)
