7846b9110ee018ad876f09706297a89ed995001c
Mezie-Gift <iammeziegift@gmail.com>
2022-12-07 14:23:25 -0800

    print squares of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,6 @@
 def square_matrix_simple(matrix=[]):
     """This computes the square value of all integers of a matrix"""
 
-
     new = []
     for i in matrix:
         new.append([j ** 2 for j in i])
