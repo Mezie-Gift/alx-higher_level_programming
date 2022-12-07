2d15bdf1964d4a0412513b3e87d2a6e7348d1f26
Mezie-Gift <iammeziegift@gmail.com>
2022-12-07 11:55:03 -0800

    square of a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    """This computes the square value of all integers of a matrix"""
+
+
+    matrix_sqr = matrix.copy()
+    for i in range(len(matrix)):
+        matrix_sqr[i] = list(map(lambda x: x**2, matrix[i]))
+    return (matrix_sqr)
