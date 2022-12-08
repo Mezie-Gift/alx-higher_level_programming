f4ec10f26da1898f5245fefc148606d79708c110
Mezie-Gift <iammeziegift@gmail.com>
2022-12-08 03:33:31 -0800

    update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# AUTHOR: Mezie Gift
+def update_dictionary(a_dictionary, key, value):
+    """Function replaces or adds key/value in a dictionary"""
+
+#    for i in range(len(a_dictionary)):
+ #       if i == key:
+#         new_value = i = value
+#       else:
+    a_dictionary[key] = value
+    return a_dictionary
