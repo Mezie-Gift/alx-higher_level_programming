87d0aa5c7c6ca2a75f6481850a06afedcb18db22
Mezie-Gift <iammeziegift@gmail.com>
2022-12-08 03:36:32 -0800

    update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,9 +3,6 @@
 def update_dictionary(a_dictionary, key, value):
     """Function replaces or adds key/value in a dictionary"""
 
-#    for i in range(len(a_dictionary)):
- #       if i == key:
-#         new_value = i = value
-#       else:
+
     a_dictionary[key] = value
     return a_dictionary
