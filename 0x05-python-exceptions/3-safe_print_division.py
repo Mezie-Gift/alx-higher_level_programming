d6b236e5eef0a88221d158e58807cba7f252f5b9
Mezie-Gift <iammeziegift@gmail.com>
2022-12-19 10:36:46 -0800

    integer division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# AUTHOR: Mezie Gift
+def safe_print_division(a, b):
+    """function  divides 2 integers and prints the result """
+    ans = 0
+    try:
+        ans = a / b
+    except ZeroDivisionError:
+        ans = None
+    finally:
+        print("inside result: {}".format(ans))
+        return (ans)
