7b17eccdbe3d3b8631dbc7035ec03e4014e52478
Mezie-Gift <iammeziegift@gmail.com>
2022-12-23 16:06:25 -0800

    return sum

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# AUTHOR: Mezie Gift
+def add_integer(a, b=98):
+    """add_integer adds number a and b, and returns the result.
+
+    if a or b is neither an integer nor float, function raises a TypeError message: a must be an integer/ b must be an integer, respectively.
+
+    if either a or b is a float, it will be first  casted to integer before the sum is returned.
+    """
+    if (not type(a) is int):
+        if (not type(a) is float):
+            raise TypeError("a must be an integer")
+    if (not type(b) is int):
+        if (not type(b) is float):
+            raise TypeError("b must be an integer")
+    result = int(a) + int(b)
+    return (result)
