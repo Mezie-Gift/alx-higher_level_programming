c0c9dcc10d94f6b0c8b48e624720e66dd0b29533
Mezie-Gift <iammeziegift@gmail.com>
2022-12-27 08:12:56 -0800

    print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,32 @@
+#!/usr/bin_python3
+# AUTHOR: Mezie Gift
+"""Define print_square"""
+
+def print_square(size):
+    """Function prints a square with the character '#'
+    Args:
+       size:is the size length of the square
+    Errors:
+       if size is not an integer:
+           raise TypeError(size must be an integer)
+       if size is less than 0:
+           raise TypeError(size must be >= 0)
+       if size is a float and is less than 0:
+           raise TypeError(size must be an integer)
+    """
+    if not isinstance(size, int):
+        if not isinstance(size, float):
+            raise TypeError("size must be an integer")
+    if (size < 0):
+        raise ValueError("size must be >= 0")
+    if (isinstance(size, float)):
+        if (size < 0):
+           raise TypeError("size must be an integer")
+        else:
+           for i in range(0, int(size)):
+               print("{}".format("#" * int(size)), end="")
+               print("")
+    else:
+        for i in range(0, int(size)):
+            print("{}".format("#" * size), end="")
+            print("")
