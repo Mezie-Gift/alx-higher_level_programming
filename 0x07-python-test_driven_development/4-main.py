d2e3749ffc32a674e43629ab43ef302eab67d002
Mezie-Gift <iammeziegift@gmail.com>
2023-01-04 07:15:03 -0800

    print square

--- 0x07-python-test_driven_development/4-main.py
+++ 0x07-python-test_driven_development/4-main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 print_square = __import__('4-print_square').print_square
 
-print_square(3.5)
+print_square(4)
 print("")
 print_square(10)
 print("")
-print_square('#')
+print_square(0)
 print("")
 print_square(1)
 print("")
