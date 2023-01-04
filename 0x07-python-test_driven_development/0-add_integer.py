91390441b48ff03bdde191e4ff55d9b1d077c133
Mezie-Gift <iammeziegift@gmail.com>
2023-01-04 06:03:48 -0800

    integer additions

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,12 +2,16 @@
 # AUTHOR: Mezie Gift
 """Define integer addition function"""
 
+
 def add_integer(a, b=98):
+
     """add_integer adds number a and b, and returns the result.
 
-    if a or b is neither an integer nor float, function raises a TypeError message: a must be an integer/ b must be an integer, respectively.
+    if a or b is neither an integer nor float,
+    function raises a TypeError message.
 
-    if either a or b is a float, it will be first  casted to integer before the sum is returned.
+    if either a or b is a float, it will be first
+    casted to integer before the sum is returned.
     """
     if (not type(a) is int):
         if (not type(a) is float):
