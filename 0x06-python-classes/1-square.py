bdef6e02fc9df55475c9cde634faf42c89efa43a
Mezie-Gift <iammeziegift@gmail.com>
2023-01-07 08:44:44 -0800

    initialize square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,9 @@
-#!/usr/python3
+#!/usr/bin/python3
 # AUTHOR: Mezie Gift
 """A square class"""
 
 
 class Square:
-    """Defines a square based on 0-square.py"""
+    """initializes a square based on 0-square.py"""
     def __init__(self, size):
         self.__size = size
