b107a5ed07f2aaff36de23d6653718fc33bf45e9
Mezie-Gift <iammeziegift@gmail.com>
2023-01-08 11:09:50 -0800

    square class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# AUTHOR: Mezie Gift
+"""Defines a square class"""
+
+
+class Square:
+    """Square defines a square based on 1-square.py"""
+    def __init__(self, size=0):
+        self.__size = size
+        """A TypeError is raised if size is not of type int.
+        A ValueError is raised if size is less than 0.
+        """
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
