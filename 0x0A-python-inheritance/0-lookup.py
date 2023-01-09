0fc209f5b4f6b73284ad92685f050db0c2f14415
Mezie-Gift <iammeziegift@gmail.com>
2023-01-09 14:48:38 -0800

    return attributes

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# AUTHOR: Mezie Gift
+"""module returns atrributes of an object"""
+
+
+def lookup(obj):
+    """returns the list of available attributes and methods of an object"""
+    return dir(obj)
