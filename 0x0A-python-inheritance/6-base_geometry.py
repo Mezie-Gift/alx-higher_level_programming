17861a059032b30f5a66837191ffcc95c4f3e5c0
Mezie-Gift <iammeziegift@gmail.com>
2023-01-10 03:35:12 -0800

    raise Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# AUTHOR: Mezie Gift
+"""Module defines a geometry class"""
+
+
+class BaseGeometry(object):
+    """class defines the method 'area'"""
+    def area(self):
+        raise Exception("area() is not implemented")
