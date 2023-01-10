507838bd62dbbe7ea0ac895b00f98098800f4789
Mezie-Gift <iammeziegift@gmail.com>
2023-01-10 10:55:19 -0800

    rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# AUTHOR: Mezie Gift
+"""Module defines a BaseaGeometry class that is the parent of rectangle class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class defines a rectangle"""
+    def __init__(self, width, height):
+        """class defines attributes of a rectangle
+        Args:
+        (int)width: width of the rectangle
+        (int)height: height of the rectangle
+        """
+        self.__width = width
+        self.__height = height
