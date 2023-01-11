de800ee071caf852d40c646e547e74d21e838c57
Mezie-Gift <iammeziegift@gmail.com>
2023-01-11 15:33:00 -0800

    json.dump()

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/use/bin/python3
+# AUTHOR: Mezie Gift
+"""Mode defines to_json_string function"""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string)
+    Args:
+        my_obj: the object to return in jason format
+    """
+    js = my_obj
+    return json.dumps(js)
