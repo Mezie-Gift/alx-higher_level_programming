41c4169a5b228c33e0cafe21c96a6760204521d2
Mezie-Gift <iammeziegift@gmail.com>
2023-01-11 08:59:25 -0800

    return square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/puthon3
+# AUTHOR: Mezie Gift
+"""Module defines a class 'Square' that inherits
+ from Rectangle (9-rectangle.py)"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class represents a square based on Super.Rectangle"""
+    def __init__(self, size):
+        """initializing attributes of the square
+        Args:
+        size: size of the square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Return the print() and str() representation of a Rectangle."""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__size) + "/" + str(self.__size)
+        return string
