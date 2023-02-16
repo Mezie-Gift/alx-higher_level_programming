db713e543cd63be18cf05a756cf79b0ac43f9376
Mezie-Gift <iammeziegift@gmail.com>
2023-02-16 03:39:33 -0800

    Create database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- This creates the database `hbtn_0c_0` in MySQl server
+-- if the database `hbtn_0c_0` already exists, it is overwritten
+DROP DATABASE IF EXISTS hbtn_0c_0;
+CREATE DATABASE hbtn_0c_0;
