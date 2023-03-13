7c672cc8bef69a026da70ad63919c4efdee87b23
Mezie-Gift <iammeziegift@gmail.com>
2023-03-13 14:03:37 -0700

    multi lingual

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* This script prints three lines:
+   `C is fun`
+   `python is cool`
+   `JavaScript is amazing`
+ */
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+console.log(lang[0]);
+console.log(lang[1]);
+console.log(lang[2]);
