a65e4c1c0fb55e1d2fc5964fe819def28c23a87a
Mezie-Gift <iammeziegift@gmail.com>
2023-03-14 07:42:52 -0700

    pass to integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+/* This script prints: `My number: <first argument converted in integer>` if the first argument can be converted to an integer:
+  - If the argument cannot be converted to an integer, print `Not a number`.
+ */
+
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else if (process.argv[2]) {
+  if (Number.isInteger(process.argv[2]) === true) {
+    console.log(process.argv[2]);
+  } else if (typeof process.argv[2] === 'number' && Number.isInteger(process.argv[2]) !== true) {
+    console.log(parseInt(process.argv[2]));
+  } else {
+    if (typeof process.argv[2] === 'string' && isNaN(process.argv[2])) {
+      console.log('Not a number');
+    } else if (typeof process.argv[2] === 'string' && typeof parseInt(process.argv[2]) === 'number') {
+      console.log(parseInt(process.argv[2]));
+    }
+  }
+}
