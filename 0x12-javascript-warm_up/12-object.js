a77d7ed06574a57b2757d62822e73f904dd7089b
Mezie-Gift <iammeziegift@gmail.com>
2023-03-15 10:33:54 -0700

    updated version

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/*
+ This script replaces the value 12 with 89
+ */
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
