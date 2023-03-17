77adf6b34df663669cb88d40d82b5f0e4a8ed8cf
Mezie-Gift <iammeziegift@gmail.com>
2023-03-17 04:30:35 -0700

    reverse an array

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/* This function returns the reversed version of a list.
+ */
+
+exports.esrever = function (list) {
+  let newList = [];
+
+  for (let i = 1; i <= list.length; i++) {
+    if (i === list.length){
+      for(let j = list.length - 1; j >= 0; j--) {
+        newList.push(list[j]);
+      }
+    }
+  }
+  return newList;
+}
