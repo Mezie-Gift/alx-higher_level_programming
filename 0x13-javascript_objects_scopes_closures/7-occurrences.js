ff2896b3e066d68b925574af486b83feae112531
Mezie-Gift <iammeziegift@gmail.com>
2023-03-17 03:37:33 -0700

    search for an occurance

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/* This function returns the number of occurrences in a list.
+ */
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count += 1;
+    }
+  }
+  return count;
+};
