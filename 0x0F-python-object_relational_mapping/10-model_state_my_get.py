5ff45f639dd2c0620ded3f81152df1094a853a7c
Mezie-Gift <iammeziegift@gmail.com>
2023-03-22 06:12:43 -0700

    print state id

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -9,7 +9,8 @@ from model_state import Base, State
 
 if __name__ == "__main__":
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'                                                                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))                                      
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
