c84a93068529766f9c46a522fd76811528e9c933
Mezie-Gift <iammeziegift@gmail.com>
2023-03-22 05:42:42 -0700

    filter by a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This script lists all objects from database `hbtn_0e_6_usa`
+"""
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for item in session.query(State).order_by(State.id):
+        if "a" in item.name:
+            print("{}: {}".format(item.id, item.name))
