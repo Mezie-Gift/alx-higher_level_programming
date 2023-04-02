629f99e3f41b545d467bd4c973e930f20ea7a26e
Mezie-Gift <iammeziegit@gmail.com>
2023-04-01 17:42:46 -0700

    get Content-Length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-#Takes in a URL, sends a request to it, and displays the
-#+size of the body of the response.
-
-response=$(curl -sI "$1$2")
-content_length=$(echo "$response" | grep -i content-length | awk '{print $2}')
-echo "$content_length"
+#This script displays the `Content_Length` of an HTTP request
+response=$(curl -sI "$1$2") && content_length=$(echo "$response" | grep -i content-length | awk '{print $2}') && echo "$content_length"
