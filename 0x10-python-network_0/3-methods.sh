17d5d8b5c3695e826ba6c0c6179ea3ec4cf31d2f
Mezie-Gift <iammeziegit@gmail.com>
2023-04-02 08:01:37 -0700

    request OPTIONS

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This script takes in an URL and displays all the HTTP methods the server would accept.
-curl -sX OPTIONS "$1"
+options=$(curl -sI OPTIONS "$1") && allowed_methods=$(echo "$options" | grep "Allow:" | sed 's/Allow: //') && echo "$allowed_methods"
