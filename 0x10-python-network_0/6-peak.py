4025bdc3c33388b4cc3e058b03a4707533068db7
Mezie-Gift <iammeziegit@gmail.com>
2023-04-03 06:45:41 -0700

    Peak search

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This function finds a peak in a list of unsorted integers"""
+from scipy.signal import find_peaks
+
+
+def find_peak(list_of_integers):
+    n = len(list_of_integers)
+    if n == 0:
+        return None
+    if n == 1:
+        return list_of_integers[0]
+    if n == 2:
+        return max(list_of_integers)
+
+    mid = n // 2
+    if list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    elif list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid + 1:])
+    else:
+        return list_of_integers[mid]
