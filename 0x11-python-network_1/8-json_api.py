cb46e61f95c97f118d427b37aceecdebfc69eec5
Mezie-Gift <iammeziegit@gmail.com>
2023-04-06 21:25:26 -0700

    json API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,7 @@ import requests
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     if len(argv) == 1:
-        q = ""
+        load = {"q": ""}
     else:
         argv[1].isalpha()
         load = {"q": argv[1]}
